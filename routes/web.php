099ed21a48dfa68535eea36a9666a2695e3e7a67
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-12-18 23:10:03 -0600

    Feat: agregar plantilla

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/prueba', function () {
+    return Inertia::render('Prueba');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

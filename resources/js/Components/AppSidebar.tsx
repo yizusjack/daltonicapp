a22238cbc2042c7f9de9d3c5827be78621e649d1
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-12-21 13:50:35 -0600

    Feat: agregar footer para el sidebar

--- resources/js/Components/AppSidebar.tsx
+++ resources/js/Components/AppSidebar.tsx
@@ -12,6 +12,8 @@ import {
 import { ChevronUp, LogOut, User2, UserPen } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
 import { Link, usePage } from '@inertiajs/react';
+import DropdownLogin from './partials/DropdownLogin';
+import LoginButtons from './partials/LoginButtons';
 
 export default function AppSidebar() {
     const user = usePage().props.auth.user;
@@ -26,41 +28,11 @@ export default function AppSidebar() {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         {user ? (
-                            <>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <SidebarMenuButton className='text-md hover:bg-[#b5b7bd]'>
-                                            <User2 /> {user.name}
-                                            <ChevronUp className="ml-auto" />
-                                        </SidebarMenuButton>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent
-                                        side="top"
-                                        className="w-[--radix-popper-anchor-width] bg-white py-2 px-3 mb-1 rounded-md"
-                                    >
-                                        <Link href={route('profile.edit')}>
-                                            <div className='p-1 rounded-sm hover:bg-slate-100'>
-
-                                                <div className="inline-flex">
-                                                    <UserPen className='h-4 m-auto' />
-                                                    Editar perfil
-                                                </div>
-                                            </div>
-                                        </Link>
-                                        <div className='p-1 rounded-sm hover:bg-slate-100'>
-                                            <div className="inline-flex">
-                                                <LogOut className='h-4 m-auto' />
-                                                Cerrar sesión
-                                            </div>
-                                        </div>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </>)
-                            : (
-                                <>
-                                    Inicia sesión
-                                </>
-                            )}
+                            <DropdownLogin user={user} />
+                        )
+                        : (
+                            <LoginButtons />
+                        )}
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarFooter>

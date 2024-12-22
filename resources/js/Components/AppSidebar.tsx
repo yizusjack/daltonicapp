ce3c3c570ac80ec95ce2acbd7edcf402c9c8e673
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-12-21 18:08:57 -0600

    Feat: sidebar

--- resources/js/Components/AppSidebar.tsx
+++ resources/js/Components/AppSidebar.tsx
@@ -4,13 +4,15 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupAction,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/Components/ui/sidebar"
-import { ChevronUp, LogOut, User2, UserPen } from 'lucide-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Camera, CircleHelp, ClipboardX, Diff, GalleryHorizontal, Images, Speech, UsersRound, View } from 'lucide-react'
 import { Link, usePage } from '@inertiajs/react';
 import DropdownLogin from './partials/DropdownLogin';
 import LoginButtons from './partials/LoginButtons';
@@ -19,10 +21,66 @@ export default function AppSidebar() {
     const user = usePage().props.auth.user;
     return (
         <Sidebar>
-            <SidebarHeader />
+            <SidebarHeader>
+                <div className='text-center text-2xl font-extrabold	'>
+                    DALTONICAPP
+                </div>
+            </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                <SidebarGroup>
+                    <SidebarGroupLabel>Cámara</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <Camera />Nueva imagen
+                        </SidebarMenuButton>
+                        <SidebarMenuButton>
+                            <Images />Mi galería
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>Foro</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <CircleHelp />Dudas
+                        </SidebarMenuButton>
+                        <SidebarMenuButton>
+                            <Speech />Comunidad
+                        </SidebarMenuButton>
+                        <SidebarMenuButton>
+                            <GalleryHorizontal />Imágenes para mi
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>Guía de contribución</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <View /> Ayuda a mejorar test de visión
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>Administración</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <UsersRound /> Usuarios
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <Diff /> Permisos
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                    <SidebarGroupContent>
+                        <SidebarMenuButton>
+                            <ClipboardX /> Reportes
+                        </SidebarMenuButton>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
                 <SidebarMenu>
@@ -30,9 +88,9 @@ export default function AppSidebar() {
                         {user ? (
                             <DropdownLogin user={user} />
                         )
-                        : (
-                            <LoginButtons />
-                        )}
+                            : (
+                                <LoginButtons />
+                            )}
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarFooter>

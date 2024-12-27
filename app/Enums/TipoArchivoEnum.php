b67b62e2c7b017890b7a9947cf4e1fb4fb430f45
Jesús Pérez Hernández <yizussawyer@gmail.com>
2024-12-26 23:48:28 -0600

    Feat: crear enum para tipo de archivos

---
+++ app/Enums/TipoArchivoEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use App\Traits\EnumToArray;
+
+/**
+ * Enumerado para mostrar los tipos de archivos validos.
+ *
+ * @enum
+ */
+enum TipoArchivoEnum: string
+{
+    use EnumToArray;
+
+    case Ishihara = 'Imagen Ishihara';
+}

8fa7832ee3560234498ace0e7bca45553afe7b89
Jesús Pérez Hernández <yizussawyer@gmail.com>
2025-01-02 23:02:53 -0600

    Feat: relaciones entre User y GuiaContribucion

--- app/Models/GuiaContribucion.php
+++ app/Models/GuiaContribucion.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\GuiaContribucion;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GuiaContribucion extends Model
 {
@@ -11,4 +14,18 @@ class GuiaContribucion extends Model
     use HasFactory;
 
     protected $table = 'guia_contribuciones';
+
+    protected $fillable = [
+        'user_id'
+    ];
+
+    /**
+     * Una GuiaContibucion tiene un usuario
+     * 
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\GuiaContribucion;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -44,5 +46,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Un usuario pertenece a una GuiaContribucion
+     * 
+     * @return HasOne
+     */
+    public function guiaContribucion(): HasOne
+    {
+        return $this->hasOne(GuiaContribucion::class);
     }
 }

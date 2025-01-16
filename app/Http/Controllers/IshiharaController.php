a13cab0e474f29e9a6ee5aea815169f8fea3887b
Jesús Pérez Hernández <yizussawyer@gmail.com>
2025-01-15 23:39:26 -0600

    Feat: rutas y controlador para obtener tipo de daltonismo

---
+++ app/Http/Controllers/IshiharaController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IshiharaController extends Controller
+{
+    /**
+     * Formulario para test de ishihara
+     */
+    public function test()
+    {
+
+    }
+
+    /**
+     * Obtiene el tipo de daltonismo y lo guarda en el usuario
+     */
+    public function getTipoDaltonismo()
+    {
+        
+    }
+}

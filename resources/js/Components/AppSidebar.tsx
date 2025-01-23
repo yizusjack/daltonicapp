e5c370b3e558422bdb43e446500564f5b866504f
Jesús Pérez Hernández <yizussawyer@gmail.com>
2025-01-22 23:48:35 -0600

    Feat: modificar sidebar

--- resources/js/Components/AppSidebar.tsx
+++ resources/js/Components/AppSidebar.tsx
@@ -12,14 +12,14 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from "@/Components/ui/sidebar"
-import { Camera, CircleHelp, ClipboardX, Diff, GalleryHorizontal, Images, Speech, UsersRound, View } from 'lucide-react'
+import { Camera, CircleHelp, ClipboardX, Diff, GalleryHorizontal, Images, ScanEye, Speech, UsersRound, View } from 'lucide-react'
 import { Link, usePage } from '@inertiajs/react';
 import DropdownLogin from './partials/DropdownLogin';
 import LoginButtons from './partials/LoginButtons';
 import Can from './Auth/Can';
 
 export default function AppSidebar() {
-    const user = usePage().props.auth.user;
+    const user = usePage().props.auth?.user;
     const permissions = usePage().props.auth?.permissions;
     const { url } = usePage();
 
@@ -31,7 +31,7 @@ export default function AppSidebar() {
                 </div>
             </SidebarHeader>
             <SidebarContent>
-                <Can permission={! permissions.imagenes.useCamera}>
+                <Can permission={permissions ? (! permissions.imagenes.useCamera) : false}>
                     <SidebarGroup>
                         <SidebarGroupLabel>Test de daltonismo</SidebarGroupLabel>
                         <SidebarGroupContent>
@@ -45,7 +45,7 @@ export default function AppSidebar() {
                 </Can>
 
                 <Can
-                    permission={permissions.imagenes.useCamera}
+                    permission={permissions ? (permissions.imagenes.useCamera) : false}
                 >
                     <SidebarGroup>
                         <SidebarGroupLabel>Cámara</SidebarGroupLabel>
@@ -70,7 +70,7 @@ export default function AppSidebar() {
                             <Speech />Comunidad
                         </SidebarMenuButton>
                         <Can
-                            permission={permissions.imagenes.useCamera}
+                            permission={permissions ? (permissions.imagenes.useCamera) : false}
                         >
                             <SidebarMenuButton>
                                 <GalleryHorizontal />Imágenes para mi
@@ -79,7 +79,7 @@ export default function AppSidebar() {
                     </SidebarGroupContent>
                 </SidebarGroup>
 
-                <Can permission={permissions.guiaContribucion.create && permissions.imagenes.useCamera}>
+                <Can permission={permissions ? (permissions.guiaContribucion.create && permissions.imagenes.useCamera) : false}>
                     <SidebarGroup>
                         <SidebarGroupLabel>Guía de contribución</SidebarGroupLabel>
                         <SidebarGroupContent>
@@ -108,6 +108,13 @@ export default function AppSidebar() {
                         <SidebarMenuButton>
                             <ClipboardX /> Reportes
                         </SidebarMenuButton>
+                    </SidebarGroupContent>
+                    <SidebarGroupContent>
+                        <Link href='/imagenes'>
+                            <SidebarMenuButton isActive={url === '/imagenes'}>
+                                <ScanEye /> Imágenes de test
+                            </SidebarMenuButton>
+                        </Link>
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>

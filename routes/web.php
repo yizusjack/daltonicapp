d3a2d734384773a885ed0ef1041f4f592ca5c88d
Jesús Pérez Hernández <yizussawyer@gmail.com>
2025-01-26 15:07:54 -0600

    Feat: agregar componente con la nueva plantilla

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/prueba', function () {
     return Inertia::render('Prueba');
 })->name('prueba');
 
+Route::get('/log', function () {
+    return Inertia::render('Auth/LoginPage');
+})->name('log');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

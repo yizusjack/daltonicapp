40023ac1c655cf5bd1bbfe2abefc1fbbd352f7e4
Jesus Perez Hernandez <yizussawyer@gmail.com>
2025-03-11 23:39:52 -0600

    Feat: agregar archivos de reporte

---
+++ app/Http/Controllers/ReporteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReporteRequest;
+use App\Http\Requests\UpdateReporteRequest;
+use App\Models\Reporte;
+
+class ReporteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReporteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reporte $reporte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Reporte $reporte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReporteRequest $request, Reporte $reporte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reporte $reporte)
+    {
+        //
+    }
+}

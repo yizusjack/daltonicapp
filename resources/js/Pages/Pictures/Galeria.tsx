21da8543f79409556ea45e0e4d76524bd7b16604
Jesus Perez Hernandez <yizussawyer@gmail.com>
2025-04-22 15:31:02 -0600

    Feat: editar publicación

--- resources/js/Pages/Pictures/Galeria.tsx
+++ resources/js/Pages/Pictures/Galeria.tsx
@@ -11,6 +11,11 @@ import { Button } from '@/Components/ui/button';
 import { Dialog, DialogContent } from '@/Components/ui/dialog';
 import Can from '@/Components/Auth/Can';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/Components/ui/dropdown-menu';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/Components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+import { router, useForm } from '@inertiajs/react';
+import { FormProvider, useForm as useFormContext } from 'react-hook-form';
+
 
 export default function Galeria({
     imagenes
@@ -32,12 +37,51 @@ export default function Galeria({
     const [abrirModal, setAbrirModal] = useState(false);
     const [selectedPicture, setSelectedPicture] = useState<PublicacionWithRelations | null>(null);
 
+    const [editarPublicacion, setEditarPublicacion] = useState(false);
+
     const mostrarImagen = (picture: PublicacionWithRelations) => {
+        setEditarPublicacion(false);
+        reset();
         setSelectedPicture(picture);
 
         setTimeout(() => {
             setAbrirModal(true);
         }, 10);
+    }
+
+    //Forms para la creación de publicación
+    const { data, setData, put, processing, errors, reset } = useForm<{
+        contenido: string;
+    }>({
+        contenido: '',
+    });
+
+    const methods = useFormContext({
+        defaultValues: {
+            contenido: data.contenido,
+        },
+    });
+
+    const cargarEdicion = () => {
+        data.contenido = selectedPicture?.contenido ?? '';
+        setEditarPublicacion(true);
+    }
+
+    const submitEdicion = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        put(route('publicacion.update', selectedPicture?.id), {
+            preserveScroll: true,
+            onSuccess: () => {
+                // let selected = selectedPicture;
+                if(selectedPicture) {
+                    selectedPicture.contenido = data.contenido;
+                }
+
+                reset();
+                setEditarPublicacion(false);
+            },
+        });
     }
 
     return (
@@ -108,36 +152,36 @@ export default function Galeria({
                                             permission={selectedPicture.canEditar || selectedPicture.canEliminar}
                                         >
                                             {/* <div className="justify-end"> */}
-                                                <DropdownMenu>
-                                                    <DropdownMenuTrigger className='hover:bg-slate-200 rounded-md p-0.5'>
-                                                        <EllipsisVertical className='w-4 h-4' />
-                                                    </DropdownMenuTrigger>
-                                                    <DropdownMenuContent>
-                                                        <Can
-                                                            permission={selectedPicture.canEditar}
+                                            <DropdownMenu>
+                                                <DropdownMenuTrigger className='hover:bg-slate-200 rounded-md p-0.5'>
+                                                    <EllipsisVertical className='w-4 h-4' />
+                                                </DropdownMenuTrigger>
+                                                <DropdownMenuContent>
+                                                    <Can
+                                                        permission={selectedPicture.canEditar}
+                                                    >
+                                                        <DropdownMenuItem
+                                                            onClick={cargarEdicion}
                                                         >
-                                                            <DropdownMenuItem
-                                                                // onClick={() => setEditarPublicacion(publicacion)}
-                                                            >
-                                                                <Pencil className='w-3 h-3 text-green-400' />
-                                                                Editar
-                                                            </DropdownMenuItem>
+                                                            <Pencil className='w-3 h-3 text-green-400' />
+                                                            Editar
+                                                        </DropdownMenuItem>
 
-                                                            <DropdownMenuSeparator />
-                                                        </Can>
+                                                        <DropdownMenuSeparator />
+                                                    </Can>
 
-                                                        <Can
-                                                            permission={selectedPicture.canEliminar}
+                                                    <Can
+                                                        permission={selectedPicture.canEliminar}
+                                                    >
+                                                        <DropdownMenuItem
+                                                        //onClick={() => eliminarPublicacion(selectedPicture)}
                                                         >
-                                                            <DropdownMenuItem
-                                                                //onClick={() => eliminarPublicacion(selectedPicture)}
-                                                            >
-                                                                <Trash2 className='w-3 h-3 text-red-400' />
-                                                                Eliminar
-                                                            </DropdownMenuItem>
-                                                        </Can>
-                                                    </DropdownMenuContent>
-                                                </DropdownMenu>
+                                                            <Trash2 className='w-3 h-3 text-red-400' />
+                                                            Eliminar
+                                                        </DropdownMenuItem>
+                                                    </Can>
+                                                </DropdownMenuContent>
+                                            </DropdownMenu>
                                             {/* </div> */}
                                         </Can>
                                     </div>
@@ -145,9 +189,56 @@ export default function Galeria({
                                     <div className='italic'>{selectedPicture.fecha}</div>
                                 </div>
 
-                                <div className="text-justify whitespace-pre-line">
-                                    "{selectedPicture.contenido}"
-                                </div>
+                                {
+                                    !editarPublicacion ? (
+                                        <>
+                                            <div className="text-justify whitespace-pre-line">
+                                                "{selectedPicture.contenido}"
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <FormProvider {...methods}>
+                                                <form onSubmit={submitEdicion}>
+                                                    <div className='pb-4'>
+                                                        <FormField
+                                                            name="contenido"
+                                                            render={({ field }) => (
+                                                                <FormItem>
+                                                                    <FormControl>
+                                                                        <Textarea
+                                                                            className='h-20'
+                                                                            name='contenido'
+                                                                            value={data.contenido}
+                                                                            onChange={(e) => setData('contenido', e.target.value)}
+                                                                        />
+                                                                    </FormControl>
+
+                                                                    {errors.contenido && (
+                                                                        <FormMessage>{errors.contenido}</FormMessage>
+                                                                    )}
+                                                                </FormItem>
+                                                            )}
+                                                        >
+                                                        </FormField>
+                                                    </div>
+
+                                                    <div className="flex justify-end gap-x-4">
+                                                        <Button
+                                                            type='button'
+                                                            variant='outline'
+                                                            onClick={() => setEditarPublicacion(false)}
+                                                        >
+                                                            Cancelar
+                                                        </Button>
+                                                        <Button>
+                                                            Guardar
+                                                        </Button>
+                                                    </div>
+                                                </form>
+                                            </FormProvider>
+                                        </>)
+                                }
                             </div>
                         </div>
                     </DialogContent>

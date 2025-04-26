d6ad2355c56fda02c47fc4c5801ce98b845cf4d7
Jesus Perez Hernandez <yizussawyer@gmail.com>
2025-04-25 19:32:04 -0600

    Feat: animaciones

--- resources/js/Pages/Landing.tsx
+++ resources/js/Pages/Landing.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@inertiajs/react'
+import { motion } from "framer-motion";
 import React from 'react'
 
 export default function Landing() {
@@ -7,33 +8,33 @@ export default function Landing() {
             <header className="sticky top-0 z-50 bg-card shadow-md">
                 <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
                     {/* Logo o nombre del sitio */}
-                    <h1 className="text-xl font-bold text-gray-800">Daltonicapp</h1>
+                    <h1 className="opacity-0 md:opacity-100 text-2xl font-bold text-gray-800">Daltonicapp</h1>
 
                     {/* Botones */}
-                    <div className='flex justify-end'>
+                    <div className='flex justify-end font-semibold'>
                         <Link
                             href={route('login')}
                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
-                            Log in
+                            Iniciar sesión
                         </Link>
                         <Link
                             href={route('register')}
                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
-                            Register
+                            Regístrate
                         </Link>
                     </div>
                 </div>
             </header>
             <section className="relative w-full min-h-[60vh] flex items-center justify-center text-white">
                 {/* Fondo degradado multicolor */}
-                <div className="absolute inset-0 bg-gradient-to-br from-blue-900 via-yellow-300 via-red-400 to-green-800 z-1" />
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.9, ease: "easeOut" }} className="absolute inset-0 bg-gradient-to-br from-blue-900 via-yellow-300 via-red-400 to-green-800 z-1" />
                 <div className="absolute inset-0 bg-black opacity-20 z-0" />
                 {/* Contenido */}
                 <div className="z-10 text-center px-6">
-                    <h1 className="text-5xl font-bold">¡Bienvenido a Daltonicapp!</h1>
-                    <p className="mt-4 text-xl">Un lugar para ayudar con el daltonismo.</p>
+                    <motion.h1 initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.8, ease: "easeOut" }} className="text-5xl font-bold">¡Bienvenido a Daltonicapp!</motion.h1>
+                    <motion.p initial={{ x: 200, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 0.8, ease: "easeOut" }} className="mt-4 text-xl">Un lugar para ayudar con el daltonismo.</motion.p>
                 </div>
 
                 {/* Curva inferior asimétrica */}
